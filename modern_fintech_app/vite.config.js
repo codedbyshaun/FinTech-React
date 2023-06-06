5252b718ea4645f86e24730a4a92338cc1ae5895
codedbyshaun <shaun@codedbyshaun.com>
2023-06-06 14:09:51 +0200

    correct base script /repo name/

--- modern_fintech_app/vite.config.js
+++ modern_fintech_app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/modern_fintech_app/",
+  base: "/FinTech-React/",
   plugins: [react()],
 })
